41c86801de4bb7c6f47f7e164ec71e8004fb421e
Antonio Rodrigues <aaadonai@gmail.com>
2024-01-21 00:28:55 -0300

    simple page in 11ty data uses a data json file for content

---
+++ src/_data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Testing ChatGPT",
+	url: "https://testinggpt.antonioaugusto.net/",
+	language: "pt-BR",
+	description: "Pequenos projetos para testar o ChatGPT.",
+	author: {
+		name: "Antonio",
+		email: "antonio@antonioaugusto.net",
+		url: "https://antonioaugusto.net/sobre/"
+	}
+}
